14ca3b25451416f34d925b8626be2753b324e7b7
brunocghisi <brunocostaghisi@gmail.com>
2025-10-27 11:07:53 -0300

    manipulating variables

---
+++ Introduction/ch1/2Prompt.js
@@ -0,0 +1,6 @@
+name = prompt("What is your name?") //Add forms in html
+age = prompt("What is your age?")
+
+// alert("Hi there! My name is"+ name+ " and my age is " + age) //add basic pop-up
+
+console.log("Hi there! My name is"+ name+ " and my age is " + age) //Print
---
+++ Introduction/ch1/3Var.js
@@ -0,0 +1,20 @@
+let a = 20
+
+a += 20
+console.log("+ 2 "+ a)
+
+a -= 20
+console.log("- 2 "+ a)
+
+a *= 2
+console.log("* 2 "+ a)
+
+a /= 2
+console.log("/ 2 "+ a)
+
+a++
+console.log("++ "+ a)
+
+a--
+console.log("-- "+ a)
+
