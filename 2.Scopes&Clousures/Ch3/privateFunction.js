4e72409b44db07756bb2647dc4bf2ae1180fd50f
brunocghisi <brunocostaghisi@gmail.com>
2025-12-17 12:00:07 -0300

    2º book "Scopes & Clousures" CH.3: - Chapter 3 starts

---
+++ 2.Scopes&Clousures/Ch3/privateFunction.js
@@ -0,0 +1,29 @@
+function doSomething(a) {
+    b = a + doSomethingElse( a * 2 ); // 2 + (1 - (2*2))
+
+    console.log( b * 3 );
+}
+
+function doSomethingElse(a) {
+    return a - 1;
+}
+
+let b;
+
+doSomething( 2 ); // 15
+
+//
+
+function doSomething1(d) {
+    c = d + doSomethingElse1( d * 2 ); // 2 + (1 - (2*2))
+
+    console.log( c * 3 );
+
+    function doSomethingElse1(d) {
+        return d - 1;
+    }
+}
+
+let c;
+
+doSomething( 2 ); // 15
